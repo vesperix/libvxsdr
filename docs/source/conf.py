c289d235ad58656e3aafe1b0a09ecd4079ab76df
Tom Wallace <tom.wallace@vesperix.com>
2023-09-16 09:53:40 -0400

    show full version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,16 +6,10 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+# run doxygen (using the Doxyfile in this directory) to make xml
 import subprocess
 subprocess.call('doxygen', shell=True)
+
 
 # -- Project information -----------------------------------------------------
 
@@ -24,14 +18,13 @@ copyright = '2023, Vesperix Corporation'
 author = 'Vesperix Corporation'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9'
+release = '0.9.0'
 
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings.
+
 extensions = [ 'sphinx.ext.imgmath', 'breathe' ]
 
 breathe_projects = { 'vxsdr' : '../xml/' }
@@ -64,7 +57,7 @@ html_css_files = [
 ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# relative to this directory.
+
 html_static_path = ['_static']
 html_logo = '_static/vesperix_logo.png'
