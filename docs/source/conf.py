e34d71d50dacca84a6b08650d60878020cd96942
Tom Wallace <tom.wallace@vesperix.com>
2023-09-16 16:25:00 -0400

    increase patch number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = '2023, Vesperix Corporation'
 author = 'Vesperix Corporation'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.9.1'
 
 
 # -- General configuration ---------------------------------------------------
