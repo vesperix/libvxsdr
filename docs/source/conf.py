8aa7f040933ae0588f82d814adc43dfa5abe5b29
Tom Wallace <tom.wallace@vesperix.com>
2023-12-15 10:02:28 -0500

    fix Sphinx version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = '2023, Vesperix Corporation'
 author = 'Vesperix Corporation'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.5'
+release = '0.9.6'
 
 
 # -- General configuration ---------------------------------------------------
