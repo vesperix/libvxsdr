c6cc9395ca735c23e455147b39ca6f27120d2cce
Tom Wallace <tom.wallace@vesperix.com>
2024-03-21 08:24:44 -0400

    improve handling of samples per packet info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = '2024, Vesperix Corporation'
 author = 'Vesperix Corporation'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.16'
+release = '0.9.17'
 
 
 # -- General configuration ---------------------------------------------------
