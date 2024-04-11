47ace8fc66d543250e6a2a32e353c2c1aff91424
Tom Wallace <tom.wallace@vesperix.com>
2024-04-11 08:08:34 -0400

    define wire sample types

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = '2024, Vesperix Corporation'
 author = 'Vesperix Corporation'
 
 # The full version, including alpha/beta/rc tags
-release = '0.13.1'
+release = '0.14.0'
 
 # -- General configuration ---------------------------------------------------
 
