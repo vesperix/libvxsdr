6784c0f8560209666e0e9b53daa352221dafa117
Tom Wallace <tom.wallace@vesperix.com>
2024-09-03 10:49:06 -0400

    update github build settings

--- src/vxsdr_py.cpp
+++ src/vxsdr_py.cpp
@@ -29,9 +29,6 @@ template <typename T> std::vector<T> numpy_to_vector(const py::array_t<T> &np) {
 class vxsdr_py : public vxsdr {
     public:
         explicit vxsdr_py(const std::map<std::string, int64_t>& settings) : vxsdr(settings) {}
-#if defined(VXSDR_COMPILER_CLANG) || defined(VXSDR_COMPILER_APPLECLANG)
-#pragma clang diagnostic ignored "-Woverloaded-virtual"
-#endif
         size_t put_tx_data(const py::array_t<std::complex<float>, py::array::c_style | py::array::forcecast>& data_np,
                                 size_t n_requested = 0, const uint8_t subdev = 0, const double timeout_s = 10) {
             if (data_np.ndim() != 1) {
@@ -40,9 +37,6 @@ class vxsdr_py : public vxsdr {
             }
             return vxsdr::put_tx_data(numpy_to_vector<std::complex<float>>(data_np), n_requested, subdev, timeout_s);
         }
-#if defined(VXSDR_COMPILER_CLANG) || defined(VXSDR_COMPILER_APPLECLANG)
-#pragma clang diagnostic ignored "-Woverloaded-virtual"
-#endif
         size_t get_rx_data(py::array_t<std::complex<float>, py::array::c_style> data_np,
                                 size_t n_requested = 0, const uint8_t subdev = 0, const double timeout_s = 10) {
             if (data_np.ndim() != 1) {
