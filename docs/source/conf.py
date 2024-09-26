08cbcefec349bc6ae6546c4f235f4e703f184db0
Tom Wallace <tom.wallace@vesperix.com>
2024-09-26 09:03:55 -0400

    fix version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = '2024, Vesperix Corporation'
 author = 'Vesperix Corporation'
 
 # The full version, including alpha/beta/rc tags
-release = '0.19.26'
+release = '0.19.27'
 
 # -- General configuration ---------------------------------------------------
 
