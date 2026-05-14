9ff6697218dff10e3f88f275a876cb79ec88c6a0
Tom Wallace <tom.wallace@vesperix.com>
2026-05-14 14:24:42 -0400

    increment minor version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = '2026, Vesperix Corporation'
 author = 'Vesperix Corporation'
 
 # The full version, including alpha/beta/rc tags
-release = '0.20.12'
+release = '0.21.0'
 
 # -- General configuration ---------------------------------------------------
 
