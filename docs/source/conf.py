0e75ee129b8e09d79e4009dab88d3ae44d467c99
Tom Wallace <tom.wallace@vesperix.com>
2026-05-15 13:22:10 -0400

    misc cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = '2026, Vesperix Corporation'
 author = 'Vesperix Corporation'
 
 # The full version, including alpha/beta/rc tags
-release = '0.21.3'
+release = '0.21.4'
 
 # -- General configuration ---------------------------------------------------
 
